cd9e79b5291460382ea3f0ba62816a882557e817
Raisgard <borlarhjhi@gmail.com>
2025-11-11 12:59:28 +0100

    initial checkin for project 1 lesson 1

---
+++ server.js
@@ -0,0 +1,9 @@
+const express= require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+
+app.listen(PORT, () => {console.log(`Running on port ${PORT}`)});
